93b41af487f05e14be9be71d81b9d200bcb0d0dc
Noam Gal <atgardner@gmail.com>
2015-01-20 08:50:31 +0200

    added info table update to sql

--- com.atgardner.Downloader/SQLitePackager.cs
+++ com.atgardner.Downloader/SQLitePackager.cs
@@ -17,23 +17,16 @@ namespace com.atgardner.Downloader
         private static readonly String INSERT_SQL = "INSERT or REPLACE INTO tiles (x,y,z,s,image) VALUES (?,?,?,0,?)";
         private static readonly String RMAPS_TABLE_INFO_DDL = "CREATE TABLE IF NOT EXISTS info AS SELECT 99 AS minzoom, 0 AS maxzoom";
         private static readonly String RMAPS_CLEAR_INFO_SQL = "DELETE FROM info;";
-        private static readonly String RMAPS_UPDATE_INFO_MINMAX_SQL = "INSERT INTO info (minzoom,maxzoom) VALUES (?,?);";
-        private static readonly String RMAPS_INFO_MAX_SQL = "SELECT DISTINCT z FROM tiles ORDER BY z DESC LIMIT 1;";
-        private static readonly String RMAPS_INFO_MIN_SQL = "SELECT DISTINCT z FROM tiles ORDER BY z ASC LIMIT 1;";
+        private static readonly String RMAPS_UPDATE_INFO_MINMAX_SQL = "insert into info(minzoom, maxzoom) values((select min(z) from tiles), (select max(z) from tiles));";
         private static readonly string METADATA_DDL = "CREATE TABLE IF NOT EXISTS android_metadata (locale TEXT)";
         private static readonly string METADATA_SELECT = "SELECT count(*) FROM android_metadata";
         private static readonly string METADATA_INSERT = "INSERT INTO android_metadata VALUES (@locale)";
 
         private readonly DbConnection connection;
+
         public SQLitePackager(string sourceFile)
         {
             var dbFile = Path.ChangeExtension(Path.GetFileNameWithoutExtension(sourceFile), "sqlitedb");
-            //dbFile = "a.sqlitedb";
-            //if (!File.Exists(dbFile))
-            //{
-            //    SQLiteConnection.CreateFile(dbFile);
-            //}
-
             connection = new SQLiteConnection(string.Format("Data Source={0};Version=3;", dbFile));
         }
 
@@ -80,6 +73,19 @@ namespace com.atgardner.Downloader
                 command.Parameters.Add(param);
                 await command.ExecuteNonQueryAsync();
                 command.Parameters.Clear();
+            }
+        }
+
+        private async void UpdateTileMetaInfo()
+        {
+            using (var scope = connection.BeginTransaction())
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = RMAPS_CLEAR_INFO_SQL;
+                await command.ExecuteNonQueryAsync();
+                command.CommandText = RMAPS_UPDATE_INFO_MINMAX_SQL;
+                await command.ExecuteNonQueryAsync();
+                scope.Commit();
             }
         }
 
