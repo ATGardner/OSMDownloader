d290b6730dc70ab5779619af29de2097073b0236
Noam Gal <atgardner@gmail.com>
2015-03-19 08:23:13 +0200

    Added xUnit test project

---
+++ com.atgardner.OfflineMapFileGeneratorTests/BoundsTests.cs
@@ -0,0 +1,33 @@
+﻿namespace com.atgardner.OfflineMapFileGeneratorTests
+{
+    using com.atgardner.OMFG.tiles;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class BoundsTests
+    {
+        [Fact]
+        public void AddTile_SetsBounds()
+        {
+            var bounds = new Bounds(5);
+            var tile = new Tile(111, 222, 5);
+            bounds.AddTile(tile);
+            Assert.Equal(111, bounds.MinX);
+            Assert.Equal(1, bounds.Width);
+            Assert.Equal(222, bounds.MinY);
+            Assert.Equal(1, bounds.Height);
+        }
+
+        [Fact]
+        public void AddTileWithWrongZoom_ThrowsException()
+        {
+            var bounds = new Bounds(5);
+            var tile = new Tile(111, 222, 6);
+            Assert.Throws(typeof(ArgumentException), () => bounds.AddTile(tile));
+        }
+    }
+}
