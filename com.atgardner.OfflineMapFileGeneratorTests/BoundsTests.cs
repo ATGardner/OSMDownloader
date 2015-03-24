06e58c9662a182c1a6ea8fd3e7f5ff607ab3d2c0
Noam Gal <atgardner@gmail.com>
2015-03-24 09:29:55 +0200

    added another bounds test

--- com.atgardner.OfflineMapFileGeneratorTests/BoundsTests.cs
+++ com.atgardner.OfflineMapFileGeneratorTests/BoundsTests.cs
@@ -23,6 +23,20 @@
         }
 
         [Fact]
+        public void AddSeveralTiles_SetsBounds()
+        {
+            var bounds = new Bounds(2);
+            var tile = new Tile(60, -90, 2); // tile 2/1/1
+            bounds.AddTile(tile);
+            tile = new Tile(60, 90, 2); //tile 2/3/1
+            bounds.AddTile(tile);
+            Assert.Equal(1, bounds.MinX);
+            Assert.Equal(3, bounds.Width);
+            Assert.Equal(1, bounds.MinY);
+            Assert.Equal(1, bounds.Height);
+        }
+
+        [Fact]
         public void AddTileWithWrongZoom_ThrowsException()
         {
             var bounds = new Bounds(1);
